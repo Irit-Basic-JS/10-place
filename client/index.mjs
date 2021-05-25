2ae5963fd672fdf601269a232be75482e5bedc90
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2021-05-26 00:33:47 +0500

    Я плита

--- client/index.mjs
+++ client/index.mjs
@@ -3,22 +3,30 @@ import drawer from "./drawer.mjs";
 import picker from "./picker.mjs";
 
 document.querySelector("#start").addEventListener("submit", e => {
-  e.preventDefault();
-  main(new FormData(e.currentTarget).get("apiKey"));
-  document.querySelector(".container").classList.add("ready");
+    e.preventDefault();
+    main(new FormData(e.currentTarget).get("apiKey"));
+    document.querySelector(".container").classList.add("ready");
 });
 
 const main = apiKey => {
-  const ws = connect(apiKey);
-  ws.addEventListener("message", console.log);
+    const ws = connect(apiKey);
+    ws.addEventListener("message", message => {
+        let data = JSON.parse(message.data);
+        if (data.type === "paint" || data.type === "putPoint")
+            drawer.putArray(data.payload);
+    });
 
-  timeout.next = new Date();
-  drawer.onClick = (x, y) => {
-    drawer.put(x, y, picker.color);
-  };
+    drawer.onClick = (x, y) => {
+      ws.send(JSON.stringify({
+        type: "putPoint",
+        payload: {
+          x, y, color: picker.color
+        }
+      }));
+    };
 };
 
 const connect = apiKey => {
-  const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
-  return new WebSocket(url);
+    const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
+    return new WebSocket(url);
 };
