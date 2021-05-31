7e349580b8f367aacc97d8737e632f317ffd6030
FreeTrally <39124135+FreeTrally@users.noreply.github.com>
2021-05-31 22:38:11 +0500

    Done

--- client/index.mjs
+++ client/index.mjs
@@ -3,22 +3,39 @@ import drawer from "./drawer.mjs";
 import picker from "./picker.mjs";
 
 document.querySelector("#start").addEventListener("submit", e => {
-  e.preventDefault();
-  main(new FormData(e.currentTarget).get("apiKey"));
-  document.querySelector(".container").classList.add("ready");
+	e.preventDefault();
+	main(new FormData(e.currentTarget).get("apiKey"));
+	document.querySelector(".container").classList.add("ready");
 });
 
 const main = apiKey => {
-  const ws = connect(apiKey);
-  ws.addEventListener("message", console.log);
+	const ws = connect(apiKey);
+	ws.addEventListener("message", function (ev) {
+		const params = JSON.parse(ev.data);
+		console.log(params);
+		if (params.type === "update") {
+			drawer.put(params.payload.x, params.payload.y, params.payload.color);
+		} else if (params.type === "connection") {
+			drawer.putArray(params.payload);
+		} else if (params.type === "timeout") {
+			timeout.next = new Date(params.payload.next);
+		}
+	});
 
-  timeout.next = new Date();
-  drawer.onClick = (x, y) => {
-    drawer.put(x, y, picker.color);
-  };
+	timeout.next = new Date();
+	drawer.onClick = (x, y) => {
+		ws.send(JSON.stringify({
+			type: "putColor",
+			payload: {
+				x: x,
+				y: y,
+				color: picker.color,
+			},
+		}));
+	};
 };
 
 const connect = apiKey => {
-  const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
-  return new WebSocket(url);
+	const url = `${location.origin.replace(/^http/, "ws")}?apiKey=${apiKey}`;
+	return new WebSocket(url);
 };
--- server.mjs
+++ server.mjs
@@ -1,8 +1,9 @@
 import * as path from "path";
 import express from "express";
 import WebSocket from "ws";
-
 const port = process.env.PORT || 5000;
+
+const connections = new WeakMap();
 
 const apiKeys = new Set([
   "4a83051d-aad4-483e-8fc8-693273d15dc7",
@@ -12,51 +13,99 @@ const apiKeys = new Set([
 ]);
 
 const colors = [
-  "#140c1c",
-  "#442434",
-  "#30346d",
-  "#4e4a4e",
-  "#854c30",
-  "#346524",
-  "#d04648",
-  "#757161",
-  "#597dce",
-  "#d27d2c",
-  "#8595a1",
-  "#6daa2c",
-  "#d2aa99",
-  "#6dc2ca",
-  "#dad45e",
-  "#deeed6",
+	"#140c1c",
+	"#442434",
+	"#30346d",
+	"#4e4a4e",
+	"#854c30",
+	"#346524",
+	"#d04648",
+	"#757161",
+	"#597dce",
+	"#d27d2c",
+	"#8595a1",
+	"#6daa2c",
+	"#d2aa99",
+	"#6dc2ca",
+	"#dad45e",
+	"#deeed6",
 ];
 
 const size = 256;
-// place(x, y) := place[x + y * size]
 const place = Array(size * size).fill(null);
-for (const [colorIndex, colorValue] of colors.entries()) {
-  for (let dx = 0; dx < size; dx++) {
-    place[dx + colorIndex * size] = colorValue;
-  }
-}
 
 const app = express();
 
 app.use(express.static(path.join(process.cwd(), "client")));
 
 app.get("/*", (_, res) => {
-  res.send("Place(holder)");
+	res.send("Place(holder)");
+});
+
+app.get("api/colors", (req, res) => {
+	res.send(colors);
 });
 
 const server = app.listen(port);
 
 const wss = new WebSocket.Server({
-  noServer: true,
+	noServer: true,
+});
+
+wss.on("connection", function connection(ws) {
+	ws.on("message", function incoming(message) {
+		const data = JSON.parse(message).payload;
+		const now = new Date();
+		const connection = connections.get(ws);
+		const next = new Date(connection.next);
+		if (data.x < size && data.y < size && colors.includes(data.color) && next < now) {
+			connections.set(ws, {
+				apiKey: connection.apiKey,
+				next: new Date(now.valueOf() + 5000).toISOString(),
+			});
+			place[data.x + data.y * size] = data.color;
+			wss.clients.forEach(function (client) {
+				if (client.readyState === WebSocket.OPEN) {
+					client.send(JSON.stringify({
+						type: "update",
+						payload: {
+							x: data.x,
+							y: data.y,
+							color: data.color,
+						},
+					}));
+				}
+			});
+		} else {
+			ws.send(JSON.stringify({
+				type: "timeout",
+				next: connections.get(ws),
+			}));
+		}
+	});
+
+	const toSend = {
+		type: "connection",
+		payload: place,
+	};
+	ws.send(JSON.stringify(toSend));
 });
 
 server.on("upgrade", (req, socket, head) => {
-  const url = new URL(req.url, req.headers.origin);
-  console.log(url);
-  wss.handleUpgrade(req, socket, head, (ws) => {
-    wss.emit("connection", ws, req);
-  });
+	const url = new URL(req.url, req.headers.origin);
+	const apiKey = url.searchParams.get("apiKey");
+	if (apiKeys.has(apiKey)) {
+		wss.handleUpgrade(req, socket, head, (ws) => {
+			const now = new Date();
+			const next = new Date(now.valueOf() + 5000).toISOString();
+			connections.set(ws, {
+				apiKey: apiKey,
+				next: next,
+			});
+			wss.emit("connection", ws, req);
+
+		});
+	} else {
+		socket.destroy();
+	}
 });
