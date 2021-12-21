0f7b1d1e5c776df86234effd5fab7a2b26d049a0
angst-storm <Sergey.Kiprin@urfu.me>
2021-12-21 11:37:47 +0500

    Выполнены задания 1-12

--- client/index.mjs
+++ client/index.mjs
@@ -12,13 +12,20 @@ const main = apiKey => {
     const ws = connect(apiKey);
     ws.addEventListener("message", message => {
         let data = JSON.parse(message.data)
-        if (data.type === 'place') drawer.putArray(data.payload)
-        if (data.type === 'pick') drawer.put(data.payload.x, data.payload.y, data.payload.color);
+        if (data.type === 'timeout') {
+            timeout.next = new Date(data.payload);
+            console.log(`next time: ${new Date(data.payload).toTimeString()}`)
+        }
+        if (data.type === 'place') drawer.putArray(data.payload);
+        if (data.type === 'pick') {
+            console.log('pick')
+            drawer.put(data.payload.x, data.payload.y, data.payload.color);
+        }
     });
 
-    timeout.next = new Date();
     drawer.onClick = (x, y) => {
         ws.send(JSON.stringify({type: "pick", payload: {x: x, y: y, color: picker.color}}))
+        console.log(`pick on: ${new Date().toTimeString()}`)
     };
 };
 
--- server.mjs
+++ server.mjs
@@ -4,12 +4,12 @@ import WebSocket from "ws";
 
 const port = process.env.PORT || 5000;
 
-const apiKeys = new Set([
-    "4a83051d-aad4-483e-8fc8-693273d15dc7",
-    "c08c9038-693d-4669-98cd-9f0dd5ef06bf",
-    "4b1545c4-4a70-4727-9ea1-152ed4c84ae2",
-    "4a226908-aa3e-4a34-a57d-1f3d1f6cba84",
-]);
+const apiKeys = {
+    "4a83051d-aad4-483e-8fc8-693273d15dc7": new Date(),
+    "c08c9038-693d-4669-98cd-9f0dd5ef06bf": new Date(),
+    "4b1545c4-4a70-4727-9ea1-152ed4c84ae2": new Date(),
+    "4a226908-aa3e-4a34-a57d-1f3d1f6cba84": new Date(),
+}
 
 const colors = [
     "#140c1c",
@@ -51,7 +51,9 @@ app.get("/*", (_, res) => {
     res.send("Place(holder)");
 });
 
-const server = app.listen(port);
+const server = app.listen(port, () => {
+    console.log(`app on http://localhost:${port}/`);
+});
 
 const wss = new WebSocket.Server({
     noServer: true,
@@ -60,23 +62,37 @@ const wss = new WebSocket.Server({
 wss.on('connection', function connection(ws) {
     ws.on('message', function message(message) {
         let data = JSON.parse(message);
-        let x, y, color;
+        let x, y, color, date;
         if (data.type === "pick" &&
             0 <= (x = data.payload.x) && x <= 256 &&
             0 <= (y = data.payload.y) && y <= 256 &&
             colors.includes(color = data.payload.color)) {
-            place[data.payload.x + data.payload.y * size] = color;
-            wss.clients.forEach(client => client.send(message));
+            if ((date = new Date()) > apiKeys[keysMap.get(ws)]) {
+                console.log(`${date.toTimeString()} > ${apiKeys[keysMap.get(ws)].toTimeString()}`)
+                apiKeys[keysMap.get(ws)] = new Date(date.valueOf() + 10 * 1000);
+                place[data.payload.x + data.payload.y * size] = color;
+                wss.clients.forEach(client => client.send(message));
+            }
+            ws.send(JSON.stringify({type: "timeout", payload: apiKeys[keysMap.get(ws)].toISOString()}));
         }
     });
 
+    ws.send(JSON.stringify({type: "timeout", payload: apiKeys[keysMap.get(ws)].toISOString()}))
     ws.send(JSON.stringify({type: "place", payload: place}));
 });
+
+let keysMap = new WeakMap()
 
 server.on("upgrade", (req, socket, head) => {
     const url = new URL(req.url, req.headers.origin);
     console.log(url);
-    wss.handleUpgrade(req, socket, head, (ws) => {
-        wss.emit("connection", ws, req);
-    });
+    let apiKey = url.searchParams.get('apiKey');
+    if (!(apiKey in apiKeys))
+        socket.destroy(new Error('wrong api key'));
+    else
+        wss.handleUpgrade(req, socket, head, (ws) => {
+            keysMap.set(ws, apiKey);
+            wss.emit("connection", ws, req);
+        });
+
 });
